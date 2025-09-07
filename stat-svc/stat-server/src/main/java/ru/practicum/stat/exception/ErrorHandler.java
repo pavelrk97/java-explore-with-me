a91140f43e2681315d45b3d37747413c3f5a6407
pavelrk97 <pavel_r_k97@mail.ru>
2025-09-08 01:25:19 +0300

    stat_svc_all_postman добавление ДТО getStats для упрощения чтения. Дто собирается в контроллере

--- stat-svc/stat-server/src/main/java/ru/practicum/stat/exception/ErrorHandler.java
+++ stat-svc/stat-server/src/main/java/ru/practicum/stat/exception/ErrorHandler.java
@@ -4,15 +4,19 @@ import jakarta.validation.ConstraintViolationException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import lombok.extern.slf4j.Slf4j;
 
 import static org.springframework.http.HttpStatus.BAD_REQUEST;
 
+@Slf4j
 @RestControllerAdvice
 public class ErrorHandler {
 
     @ExceptionHandler(ConstraintViolationException.class)
     @ResponseStatus(BAD_REQUEST)
     public ErrorResponse handleConstraintViolationException(ConstraintViolationException ex) {
+        log.warn("Constraint violation: {}", ex.getMessage(), ex);
+
         return new ErrorResponse("BAD_REQUEST", ex.getMessage());
     }
 }
