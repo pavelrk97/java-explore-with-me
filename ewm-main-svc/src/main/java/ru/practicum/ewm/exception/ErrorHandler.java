8034fd5d456d91bbe4c87f74d532122447afa21d
pavelrk97 <pavel_r_k97@mail.ru>
2025-09-15 15:34:53 +0300

    Добавлено Логирование ErrorHandler

--- ewm-main-svc/src/main/java/ru/practicum/ewm/exception/ErrorHandler.java
+++ ewm-main-svc/src/main/java/ru/practicum/ewm/exception/ErrorHandler.java
@@ -19,64 +19,70 @@ public class ErrorHandler {
     @ExceptionHandler(ConstraintViolationException.class)
     @ResponseStatus(BAD_REQUEST)
     public ErrorResponse handleConstraintViolationException(ConstraintViolationException ex) {
+        log.error("ConstraintViolationException: {}", ex.getMessage(), ex);
         return new ErrorResponse("BAD_REQUEST", ex.getMessage());
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ErrorResponse handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+        log.error("MethodArgumentNotValidException: {}", e.getMessage(), e);
         return new ErrorResponse("Ошибка валидации: ", e.getMessage());
     }
 
     @ExceptionHandler(ValidationException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ErrorResponse handlerValidationException(ValidationException e) {
+        log.error("ValidationException: {}", e.getMessage(), e);
         return new ErrorResponse("BAD_REQUEST", e.getMessage());
     }
 
     @ExceptionHandler(NotFoundException.class)
     @ResponseStatus(NOT_FOUND)
     public ErrorResponse handleNotFoundException(NotFoundException ex) {
+        log.error("NotFoundException: {}", ex.getMessage(), ex);
         return new ErrorResponse("NOT_FOUND", ex.getMessage());
     }
 
     @ExceptionHandler(DuplicatedDataException.class)
     @ResponseStatus(HttpStatus.CONFLICT)
     public ErrorResponse handleDuplicatedDataException(DuplicatedDataException ex) {
-        log.warn("DuplicatedDataException: {}", ex.getMessage());
+        log.warn("DuplicatedDataException: {}", ex.getMessage(), ex);
         return new ErrorResponse("CONFLICT", ex.getMessage());
     }
 
     @ExceptionHandler(ConflictException.class)
     @ResponseStatus(HttpStatus.CONFLICT)
     public ErrorResponse handleConflictException(ConflictException ex) {
-        log.warn("ConflictException: {}", ex.getMessage());
+        log.warn("ConflictException: {}", ex.getMessage(), ex);
         return new ErrorResponse("CONFLICT", ex.getMessage());
     }
 
     @ExceptionHandler(IllegalArgumentException.class)
     @ResponseStatus(BAD_REQUEST)
     public ErrorResponse handleIllegalArgumentException(IllegalArgumentException ex) {
+        log.error("IllegalArgumentException: {}", ex.getMessage(), ex);
         return new ErrorResponse("BAD_REQUEST", ex.getMessage());
     }
 
     @ExceptionHandler(IllegalStateException.class)
     @ResponseStatus(BAD_REQUEST)
     public ErrorResponse handleIllegalStateException(IllegalStateException ex) {
+        log.error("IllegalStateException: {}", ex.getMessage(), ex);
         return new ErrorResponse("BAD_REQUEST", ex.getMessage());
     }
 
     @ExceptionHandler(DataIntegrityViolationException.class)
     @ResponseStatus(HttpStatus.CONFLICT)
     public ErrorResponse handleDataIntegrityViolationException(DataIntegrityViolationException ex) {
-        log.warn("DataIntegrityViolationException: {}", ex.getMessage());
+        log.warn("DataIntegrityViolationException: {}", ex.getMessage(), ex);
         return new ErrorResponse("CONFLICT", "Нарушение уникальности данных: " + ex.getMessage());
     }
 
-    @ExceptionHandler
+    @ExceptionHandler(ForbiddenException.class)
     @ResponseStatus(HttpStatus.FORBIDDEN)
     public ApiError handleConflict(final ForbiddenException e) {
-        log.info("403 {}", e.getMessage(), e);
+        log.error("ForbiddenException: {}", e.getMessage(), e);
         return new ApiError(
                 HttpStatus.FORBIDDEN.value(),
                 "For the requested operation the conditions are not met.",
@@ -84,10 +90,10 @@ public class ErrorHandler {
         );
     }
 
-    @ExceptionHandler
+    @ExceptionHandler(IncorrectRequestException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ApiError handleIncorrectRequestException(final IncorrectRequestException e) {
-        log.info("400 {}", e.getMessage(), e);
+        log.error("IncorrectRequestException: {}", e.getMessage(), e);
         return new ApiError(
                 HttpStatus.BAD_REQUEST.value(),
                 "Incorrectly made request.",
