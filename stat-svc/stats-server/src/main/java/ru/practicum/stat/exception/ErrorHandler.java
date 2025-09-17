3946f0561165548d0dd071b2f868241d3ead3007
pavelrk97 <pavel_r_k97@mail.ru>
2025-09-17 17:35:27 +0300

    feature

--- stat-svc/stats-server/src/main/java/ru/practicum/stat/exception/ErrorHandler.java
+++ stat-svc/stats-server/src/main/java/ru/practicum/stat/exception/ErrorHandler.java
@@ -1,22 +1,50 @@
 package ru.practicum.stat.exception;
 
 import jakarta.validation.ConstraintViolationException;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestControllerAdvice;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-import static org.springframework.http.HttpStatus.BAD_REQUEST;
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
 
-@Slf4j
 @RestControllerAdvice
+@Slf4j
 public class ErrorHandler {
 
-    @ExceptionHandler(ConstraintViolationException.class)
-    @ResponseStatus(BAD_REQUEST)
-    public ErrorResponse handleConstraintViolationException(ConstraintViolationException ex) {
-        log.warn("Constraint violation: {}", ex.getMessage(), ex);
+    private ResponseEntity<ErrorResponse> buildErrorResponse(String message, HttpStatus status) {
+        ErrorResponse errorResponse = new ErrorResponse(status.name(), message, LocalDateTime.now());
+        return new ResponseEntity<>(errorResponse, status);
+    }
 
-        return new ErrorResponse("BAD_REQUEST", ex.getMessage());
+    @ExceptionHandler(ConstraintViolationException.class)
+    public ResponseEntity<ErrorResponse> handleConstraintViolationException(ConstraintViolationException ex) {
+        log.error("Ошибка валидации: {}", ex.getMessage());
+        return buildErrorResponse(ex.getMessage(), HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<ErrorResponse> handleIllegalArgumentException(IllegalArgumentException e) {
+        log.error("Ошибка: {}", e.getMessage());
+        return buildErrorResponse(e.getMessage(), HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponse> handleValidationExceptions(
+            MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getAllErrors().forEach((error) -> {
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        });
+        String errorMessage = String.join(", ", errors.values());
+        log.error("Ошибки валидации: {}", errorMessage);
+        return buildErrorResponse(errorMessage, HttpStatus.BAD_REQUEST);
     }
 }
--- stat-svc/stats-server/src/main/java/ru/practicum/stat/exception/ErrorResponse.java
+++ stat-svc/stats-server/src/main/java/ru/practicum/stat/exception/ErrorResponse.java
@@ -3,9 +3,12 @@ package ru.practicum.stat.exception;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
+import java.time.LocalDateTime;
+
 @Data
 @AllArgsConstructor
 public class ErrorResponse {
     private final String error;
     private final String message;
+    private LocalDateTime timestamp;
 }
